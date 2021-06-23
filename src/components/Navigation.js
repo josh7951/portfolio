0bb154c42acb4a9ca10f1c50c44893f7bb2191ae
Josh Mejia <jmejia7951@gmail.com>
2021-06-23 13:38:46 -0700

    Starting new portfolio using react; started sidebar nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import avatar from '../img/profilepic.jpg'
+
+function Navigation() {
+  return (
+    <NavigationStyle>
+      <div className="avatar">
+        <img src={avatar} alt="Yoo, it be me"/>
+      </div>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <NavLink to="/home" activeClassName="active">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/resume" activeClassName="active">Resume</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/projects" activeClassName="active">Projects</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" activeClassName="active">Contact Me</NavLink>
+        </li>
+      </ul>
+      <footer className="footer">
+        <p>@2021 Joshua Mejia</p>
+      </footer>
+    </NavigationStyle>
+  )
+}
+
+const NavigationStyle = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  border-right: 1px solid var(--border-color);
+
+  .avatar {
+    width: 100%;
+    border-bottom: 1px solid var(--border-color);
+    text-align: center;
+    padding: 1rem 0;
+    img{
+      width: 70%;
+      border-radius: 50%;
+      border: 8px solid var(--border-color);
+    }
+  }
+
+  .nav-items {
+    width: 100%;
+    text-align: center;
+
+    .active {
+      background-color: var(--primary-color-light);
+      color: white;
+    }
+
+    li {
+      display: block;
+      a {
+        display: block;
+        padding: .2rem 0;
+        position: relative;
+        transition: all 0.4s ease-in-out;
+        z-index: 10;
+        font-weight: 600;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        &:hover {
+          cursor: pointer;
+          color: var(--white-color);
+        }
+        &:before {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 0;
+          height: 50%;
+          background-color: var(--primary-color);
+          transition: all 0.4s cubic-bezier(1,-0.2,.25,.95);
+          opacity: 0.20;
+          z-index: -1;
+        }
+      }
+
+      a:hover::before {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  .footer {
+    border-top: 1px solid var(--border-color);
+    width: 100%;
+    p {
+      padding: 1.3rem 0;
+      font-size: 0.8rem;
+      display: block;
+      text-align: center;
+    }
+  }
+
+`;
+export default Navigation;
